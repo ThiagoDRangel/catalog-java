b3a6e9b9c2d917b16994adb0392d79493dffd71d
ThiagoDRangel <info.tec.campos@gmail.com>
2023-09-12 18:00:30 -0300

    feat: implements endpoing delete

--- backend/src/main/java/com/thiagoDRangel/catalog/services/CategoryService.java
+++ backend/src/main/java/com/thiagoDRangel/catalog/services/CategoryService.java
@@ -3,10 +3,13 @@ package com.thiagoDRangel.catalog.services;
 import com.thiagoDRangel.catalog.dto.CategoryDTO;
 import com.thiagoDRangel.catalog.entities.Category;
 import com.thiagoDRangel.catalog.repositories.CategoryRepository;
+import com.thiagoDRangel.catalog.services.exceptions.DatabaseException;
 import com.thiagoDRangel.catalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -49,6 +52,19 @@ public class CategoryService {
         }
         catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found" + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("failed! ID not found");
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation in database!");
         }
     }
 }

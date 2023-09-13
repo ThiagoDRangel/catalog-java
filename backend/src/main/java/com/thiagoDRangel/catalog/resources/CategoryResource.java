613716c7a1e3a72c6f334a4ff44f706a87f42db0
ThiagoDRangel <info.tec.campos@gmail.com>
2023-09-13 10:37:59 -0300

    fix: recover all categories with pagination

--- backend/src/main/java/com/thiagoDRangel/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/thiagoDRangel/catalog/resources/CategoryResource.java
@@ -2,6 +2,10 @@ package com.thiagoDRangel.catalog.resources;
 
 import com.thiagoDRangel.catalog.dto.CategoryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.ScrollPosition;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.thiagoDRangel.catalog.services.CategoryService;
@@ -19,8 +23,14 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> list = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,16 @@
 INSERT INTO tb_category(name, created_At) VALUES ('Books', NOW());
 INSERT INTO tb_category(name, created_At) VALUES ('Electronics', NOW());
 INSERT INTO tb_category(name, created_At) VALUES ('Computers', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Tvs', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Games', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Smartphones', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('notebooks', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('mouses', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('headsets', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Coolers', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Garden', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Seeds', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Machines', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Cars', NOW());
+INSERT INTO tb_category(name, created_At) VALUES ('Bikes', NOW());
+
